550cc35ab55316b61cde76dd398b33f3c1bb60f6
Freeman Wang <freemanwang2006@gmail.com>
2026-01-02 22:58:14 -0500

    well I might need a real db server...

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,20 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../app/generated/prisma/client";
+import path from "path";
+
+// Idk but I'm very stubborn and don't want to host a server so like I just want some readonly sqlite db is that too hard to ask for
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
-const connectionString = `${process.env.DATABASE_URL}`;
+
+let connectionString;
+if (process.env.NODE_ENV === "production") {
+  connectionString = path.join(process.cwd(), "stickers.db");
+} else {
+  connectionString = `${process.env.DATABASE_URL}`;
+}
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma =
